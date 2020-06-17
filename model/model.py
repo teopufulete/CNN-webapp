9cf5232d459d4def208b974ba0d226797d10c51c
Teo Petrea <53831298+teopufulete@users.noreply.github.com>
2020-06-17 11:05:27 +0300

    split data

--- model/model.py
+++ model/model.py
@@ -68,3 +68,13 @@ learning_rate_reduction = ReduceLROnPlateau(monitor='val_acc',
                                             factor=0.5,
                                             min_lr=0.00001)
 callbacks = [earlystop, learning_rate_reduction]
+
+
+'''Split data'''
+df["category"] = df["category"].replace({0: 'cat', 1: 'dog'})
+train_df, validate_df = train_test_split(df, test_size=0.20, random_state=42)
+train_df = train_df.reset_index(drop=True)
+validate_df = validate_df.reset_index(drop=True)
+total_train = train_df.shape[0]
+total_validate = validate_df.shape[0]
+print(df.head())
