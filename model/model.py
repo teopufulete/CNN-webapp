c98582480b17753d4313653f7dc9e96fbda79afd
Teo Petrea <53831298+teopufulete@users.noreply.github.com>
2020-06-17 10:44:12 +0300

    model architecture

--- model/model.py
+++ model/model.py
@@ -29,3 +29,30 @@ for filename in filenames:
 df = pd.DataFrame({'filename': filenames, 'category': categories})
 print(df.head())
 print(df.tail())) 
+
+'''CNN architecture'''
+model = Sequential()
+
+model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS)))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(512, activation='relu'))
+model.add(BatchNormalization())
+model.add(Dropout(0.5))
+model.add(Dense(2, activation='softmax')) # 2 because we have cat and dog classes
+
+model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+model.summary()
