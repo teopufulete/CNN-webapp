30a7d9cf67a17d31905e62c48c73f15dd76bff66
Teo Petrea <53831298+teopufulete@users.noreply.github.com>
2020-06-18 13:59:19 +0300

    add model

--- app.py
+++ app.py
@@ -3,3 +3,28 @@ import cv2
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Conv2D, MaxPooling2D, BatchNormalization, Flatten
 import numpy as np
+
+model = Sequential()
+
+model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(128,128,3)))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size = (2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(64, (3, 3), activation = 'relu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size = (2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(128, (3, 3), activation = 'relu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size = (2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(512, activation = 'relu'))
+model.add(BatchNormalization())
+model.add(Dropout(0.5))
+model.add(Dense(2, activation ='softmax'))
+model.compile(loss = 'categorical_crossentropy', optimizer = 'rmsprop', metrics = ['accuracy'])
+model.load_weights('model/model.h5')
