7514acc278d6b38ec8c1e7d449720b76a6e957a3
Teo Petrea <53831298+teopufulete@users.noreply.github.com>
2020-06-18 15:55:45 +0300

    website architecture

--- app.py
+++ app.py
@@ -28,3 +28,42 @@ model.add(Dropout(0.5))
 model.add(Dense(2, activation ='softmax'))
 model.compile(loss = 'categorical_crossentropy', optimizer = 'rmsprop', metrics = ['accuracy'])
 model.load_weights('model/model.h5')
+
+
+COUNT = 0
+app = Flask(__name__)
+app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 1
+
+@app.route('/')
+def man():
+    return render_template('index.html')
+
+
+@app.route('/home', methods=['POST'])
+def home():
+    global COUNT
+    img = request.files['image']
+
+    img.save('static/{}.jpg'.format(COUNT))
+    img_arr = cv2.imread('static/{}.jpg'.format(COUNT))
+
+    img_arr = cv2.resize(img_arr, (128,128))
+    img_arr = img_arr / 255.0
+    img_arr = img_arr.reshape(1, 128,128,3)
+    prediction = model.predict(img_arr)
+
+    x = round(prediction[0,0], 2)
+    y = round(prediction[0,1], 2)
+    preds = np.array([x,y])
+    COUNT += 1
+    return render_template('prediction.html', data = preds)
+
+
+@app.route('/load_img')
+def load_img():
+    global COUNT
+    return send_from_directory('static', "{}.jpg".format(COUNT-1))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
